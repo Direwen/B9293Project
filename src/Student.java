eb3c9659669b843d2b9c44055fa3fefd4adbcb14
Direwen <59408433+Direwen@users.noreply.github.com>
2023-06-12 09:43:28 +0630

    Version 3

--- src/Student.java
+++ src/Student.java
@@ -1,11 +1,19 @@
 
 public class Student {
-	private String name;
-	private String age;
-
+	private static String name = "Direwen";
+	private static String age = "19";
+	
+	public static String getName() {
+		return name;
+	}
+	
+	public static String getAge() {
+		return age;
+	}
+	
 	public static void main(String[] args) {
 		
-		
+		System.out.println(getName() + " is " + getAge() + "years old.");
 	}
 
 }
